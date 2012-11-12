d3b82dd8ba59c37de28bea0f02849c3d670141d9
Orlando <Orlando@DSI-Workstation>
2012-11-12 10:13:18 -0700

    =>.<=

---
+++ TaskBar/src/taskbar/TaskBar.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taskbar;
+
+/**
+ *
+ * @author Orlando
+ */
+public class TaskBar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+}
